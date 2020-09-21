72a9e1d610ed09effeb5065807762c2f4cacd5b1
Iara Isleño <iara_joana@hotmail.com>
2020-09-20 23:29:24 -0300

    exercise finished

---
+++ inheritance-01/src/ar/com/ada/online/second/inheritance01/Main.java
@@ -0,0 +1,34 @@
+package ar.com.ada.online.second.inheritance01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	    Person person1 = new Person("Matt", "Murdock");
+        Person person2 = new Person("Stephen", "Strange");
+	    Person person3 = new Person("Natasha", "Romanov");
+	    Person person4 = new Person("Carol", "Danvers");
+
+        System.out.println("person1.toString(): " + person1.toString());
+        System.out.println("person2.toString(): " + person2.toString());
+        System.out.println("person3.toString(): " + person3.toString());
+        System.out.println("person4.toString(): " + person4.toString());
+
+        System.out.println("\nMatt Murdock hashCode: " + person1.hashCode());
+        System.out.println("Stephen Strange hashCode: " + person2.hashCode());
+        System.out.println("Natasha Romanov hashCode: " + person3.hashCode());
+        System.out.println("Carol Danvers hashCode: " + person4.hashCode());
+
+        System.out.printf("\nSe compara si %s %s es igual a %s %s = %s"
+                , person1.getName(), person1.getLastName(), person2.getName(), person2.getLastName()
+                , person1.equals(person2));
+
+        System.out.printf("\nSe compara si %s %s es igual a %s %s = %s"
+                , person2.getName(), person2.getLastName(), person3.getName(), person3.getLastName()
+                , person2.equals(person3));
+
+        System.out.printf("\nSe compara si %s %s es igual a %s %s = %s"
+                , person3.getName(), person3.getLastName(), person4.getName(), person4.getLastName()
+                , person3.equals(person4));
+    }
+}
---
+++ inheritance-01/src/ar/com/ada/online/second/inheritance01/Person.java
@@ -0,0 +1,62 @@
+package ar.com.ada.online.second.inheritance01;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private String lastName;
+
+    //Constructores
+    public Person() {
+
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    //Getters & setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person { name = "
+                + this.name +
+                ", lastName = " + this.lastName + "}";
+    }
+    // También se puede escribir:
+    //String.format("Person: {name = %s, lastName = %s} ", this.name, this.lastName);
+
+    @Override
+    public int hashCode(){
+        return 20 * Objects.hash(this.name, this.lastName);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Person that = (Person) obj;
+        return name.equals(that.name) &&
+                lastName.equals(that.lastName);
+    }
+}
